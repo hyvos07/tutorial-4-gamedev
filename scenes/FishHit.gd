004986964d18885b775bfedd7ca46bd0d7f90bbb
Hyvos <daniel.liman07@gmail.com>
2026-03-06 20:52:00 +0700

    add despawn and use rigidbody benefits

--- scenes/FishHit.gd
+++ scenes/FishHit.gd
@@ -1,6 +1,12 @@
-extends Area2D
+extends RigidBody2D
 
 @export var sceneName = "LoseScreen"
+@export var despawn_time: float = 3.0
+
+func _ready():
+	await get_tree().create_timer(despawn_time).timeout
+	if is_inside_tree():
+		queue_free()
 
 func _on_FallArea_body_entered(body):
 	if body.get_name() != "Player":
